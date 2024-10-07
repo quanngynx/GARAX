bd7fafaf10bb9e10dc51afe47519329b16189c00
Truong Minh <22dh114631@st.huflit.edu.vn>
2024-10-07 08:02:23 +0700

    add commponent trong home

---
+++ GARAX-CLIENTS/main-app/src/layouts/main/components/Reason.jsx
@@ -0,0 +1,20 @@
+function Reason(){
+    return(
+        <div className="w-full flex justify-between ">
+            <div className="bg-blue-400 w-[20%] h-[80px]">
+1
+            </div>
+            <div className="bg-blue-400 w-[20%] h-[80px]">
+2
+            </div>
+            <div className="bg-blue-400 w-[20%] h-[80px]">
+3
+            </div>
+            <div className="bg-blue-400 w-[20%] h-[80px]">
+4
+            </div>
+            
+        </div>
+    );
+}
+export default Reason;
