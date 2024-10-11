226d3f153d9c8819bed82c760439b46ec3450c27
Vũ Trường <truongvunguyen3434@gmail.com>
2024-10-11 10:07:47 +0700

    làm vali email

--- GARAX-SERVER/src/controllers/access.controller.js
+++ GARAX-SERVER/src/controllers/access.controller.js
@@ -7,16 +7,13 @@ const register = async (req, res) => {
     try {
         const { email, password, fullname, phone } = req.body;
 
-<<<<<<< HEAD
         if (!email || !password) {
             return res.status(400).json({ error: 'Email and password are required' });
         }
-=======
-const register = (req, res) => {
-    console.log(req.body)
-    const { username, password } = req.body;
->>>>>>> ce1d82e2026b21b95236d729dfe09aa515b7816a
-
+        const emailRegex = /^[^\s@]+@[^\s@]+com+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({success: false, message: "Invalid email"})
+        }
         // Kiểm tra xem email đã tồn tại chưa
         const user = await User.findByEmail(email);
         if (user) {
@@ -35,29 +32,6 @@ const register = (req, res) => {
         console.error('Error registering user:', err);
         return res.status(500).json({ error: 'Internal server error' });
     }
-<<<<<<< HEAD
-=======
-    User.findByUsername(username, (err, user) => {
-        console.log(user); // Kiểm tra xem user có được tìm thấy không
-        if (err) return res.status(500).json({ error: 'Internal server error' });
-        if (user) return res.status(400).json({ error: 'Username already exists' });
-
-        bcrypt.hash(password, 10, (err, hashedPassword) => {
-            if (err) {
-                console.error("Error hashing password:", err);
-                return res.status(500).json({ error: 'Error hashing password' });
-            }
-
-            // Tạo người dùng mới
-            User.create(username , hashedPassword, (err, userId) => {
-                if (err) return res.status(500).json({ error: 'Error creating user' });
-
-                const token = jwt.sign({ IDAcc: userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
-                return res.status(201).json({ message: 'User created', token });
-            });
-        });
-    });
->>>>>>> ce1d82e2026b21b95236d729dfe09aa515b7816a
 };
 
 const login = async (req, res) => {
@@ -74,7 +48,6 @@ const login = async (req, res) => {
         console.log("Password from request:", password);
         console.log("Hashed password from DB:", user.Password);
         
-        
         // if (typeof password === 'string' && typeof user.Password === 'string') {
           //  isMatch = await bcrypt.compare(password, user.Password);
             
@@ -82,7 +55,8 @@ const login = async (req, res) => {
         // } else {
         //     console.error("Invalid input for bcrypt.compare():", password, user.Password);
         // }
-       const isMatch = await bcrypt.compare(password, user.Password);
+
+       const isMatch = bcrypt.compare(password, user.Password);
         if (isMatch) {
             // Tạo token nếu mật khẩu hợp lệ
             const token = jwt.sign({ IDAcc: user.IDAcc }, process.env.JWT_SECRET, { expiresIn: '1h' });
