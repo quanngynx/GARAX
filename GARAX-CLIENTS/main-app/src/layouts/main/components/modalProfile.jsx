0f3445744ed13d9d1a69af28d1e09081fab625da
quan3473 <nguyenminhquan042004@gmail.com>
2024-11-19 00:23:00 +0700

    .

--- GARAX-CLIENTS/main-app/src/layouts/main/components/modalProfile.jsx
+++ GARAX-CLIENTS/main-app/src/layouts/main/components/modalProfile.jsx
@@ -1,50 +1,69 @@
+
 import AnaylistIcon from "../../../assets/icons/anyalist-icon-solid.svg?react";
 import CursorEffectIcon from "../../../assets/icons/cusor-effect-icon-solid.svg?react";
 import SecurityIcon from "../../../assets/icons/security-solid.svg?react";
 import MultiAppIcon from "../../../assets/icons/multi-app-solid.svg?react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
 
-const flyoutMenusData1 = [
-  {
-    icon: <AnaylistIcon />,
-    tit: "Thông báo",
-    desc: "1",
-  },
-  {
-    icon: <MultiAppIcon />,
-    tit: "Tài khoản",
-    desc: "2",
-  },
-  {
-    icon: <SecurityIcon />,
-    tit: "Trung tâm trợ giúp",
-    desc: "3",
-  },
-  {
-    icon: <CursorEffectIcon />,
-    tit: "Đăng xuất",
-    desc: "4",
-  },
-];
+
 
 function modalProfile() {
   const navigate = useNavigate();
 
+  // const [func, setFunc] = useState("")
+
+  // const handleListFunc = () => {
+  //   setFunc()
+  // }
+
+  const navigateToProfile = () => {
+
+    navigate("/user/profile");
+  }
+
   const handleLogout = () => {
     localStorage.clear();
-    navigate('/auth/login');
+    navigate("/");
   };
+
+  const flyoutMenusData1 = [
+    {
+      icon: <AnaylistIcon />,
+      tit: "Thông báo",
+      desc: "1",
+      func: navigateToProfile(),
+    },
+    {
+      icon: <MultiAppIcon />,
+      tit: "Tài khoản",
+      desc: "2",
+      func: navigateToProfile(),
+    },
+    {
+      icon: <SecurityIcon />,
+      tit: "Trung tâm trợ giúp",
+      desc: "3",
+      func: navigateToProfile(),
+    },
+    {
+      icon: <CursorEffectIcon />,
+      tit: "Đăng xuất",
+      desc: "4",
+      func: handleLogout(),
+    },
+  ];
 
   const listCate1 = flyoutMenusData1.map((item) => (
     <div
-      key={item} 
+      key={item}
       className="group relative flex gap-x-6 rounded-lg px-4 py-1 hover:bg-gray-50"
     >
       <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
         {item.icon}
       </div>
       <button
-        onClick={item.tit === "Đăng xuất" ? handleLogout : null}
+        onClick={item.func}
         className="font-semibold text-white hover:text-gray-900"
       >
         {item.tit}
@@ -55,9 +74,7 @@ function modalProfile() {
   ));
 
   return (
-    <div
-      className="flex justify-end absolute translate-x-[-28px] left-0 right-0 z-10 mt-5 w-[98vw]"
-    >
+    <div className="flex justify-end absolute translate-x-[-28px] left-0 right-0 z-10 mt-5 w-[98vw]">
       <div className="flex flex-col overflow-hidden rounded-3xl bg-black/90 text-sm/6 shadow-lg ring-1 opacity-90 ring-gray-900/5">
         <div className="p-4">{listCate1}</div>
         <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
