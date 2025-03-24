3ecbcc6f897785dd214e1964f3a8406a6fbd0ee9
quan3473 <nguyenminhquan042004@gmail.com>
2025-03-25 01:40:47 +0700

    .

--- GARAX-SERVER/eslint.config.mjs
+++ GARAX-SERVER/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
