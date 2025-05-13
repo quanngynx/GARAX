159b8fec176b59adf4f57b0f9191d6b9020cfbf4
trungenglish <nguyentrung23232323@gmail.com>
2025-05-13 22:44:35 +0700

    #27 - add: db

---
+++ packages/garax-server/src/db/migrations/20241210000118-create-booking.js
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable('bookings', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      type: Sequelize.STRING
+    },
+    allDay: {
+      type: Sequelize.STRING
+    },
+    start: {
+      type: Sequelize.STRING
+    },
+    end: {
+      type: Sequelize.STRING
+    },
+    createdBy: {
+      type: Sequelize.STRING
+    },
+    updatedBy: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable('bookings');
+}
